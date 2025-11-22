a06944ed9855a91fe7eb9e326b068d944b005982
addisabeba287 <addisabeba287@gmail.com>
2025-11-22 09:12:33 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student student1 = new Student("AA", 20, 1.2);
+		Student student2 = new Student("bb", 12, 2.0);
+		Student student3 = new Student("cc", 8, 2.3);
+		System.out.println(student1);
+		System.out.println(student2);
+		System.out.println(student3);
+	}
+}
